d5c65f4f06d4b1b52cdbba3ee4ca2b95eba01a2f
unknown <amit.techartist@gmail.com>
2023-09-26 12:05:18 +0530

    helper push

--- src/common/Helpers.jsx
+++ src/common/Helpers.jsx
@@ -1,10 +1,10 @@
 export const app_url ="/supplyform";
 export const api_url ="https://essane.in/supply/api/";
 export const api_assets_url ="https://essane.in/supply/storage/app/public/";
-// export const local_api_url ="https://essane.in/supply/api/";
-// export const local_api_assets_url ="https://essane.in/supply/storage/app/public/";
-export const local_api_url ="http://localhost/truckform/api/";
-export const local_api_assets_url ="http://localhost/truckform/storage/app/public/";
+export const local_api_url ="https://essane.in/supply/api/";
+export const local_api_assets_url ="https://essane.in/supply/storage/app/public/";
+// export const local_api_url ="http://localhost/truckform/api/";
+// export const local_api_assets_url ="http://localhost/truckform/storage/app/public/";
 export function getCurrentDate(Date){
     let separator = '-';
     let newDate = Date;
